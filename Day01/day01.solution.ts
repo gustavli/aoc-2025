fa773f2a4bd28fb131753203297cbddf4ad26383
Gustav Lillesæter <gustav.lilleseter@sparebank1.no>
2025-12-01 08:20:22 +0100

    day 1 part 1

--- Day01/day01.solution.ts
+++ Day01/day01.solution.ts
@@ -1,11 +1,42 @@
+import { assertEquals } from "@std/assert/equals";
 import { input } from "./day01.input.ts";
 
 export const part_1 = (input: string): number => {
-    return 0
+    const lines = read_input(input)
+
+    let current_location = 50
+    let zero_indices = 0
+
+    lines.forEach(line => {
+        const direction = line.charAt(0)
+        const rotations = Number(line.slice(1)) % 100
+
+        current_location = move(current_location,direction==="L" ? "LEFT" : "RIGHT", rotations)
+
+        if(current_location === 0) zero_indices++
+    })
+
+    return zero_indices
 }
 
 export const part_2 = (input: string): number => {
     return 1
+}
+
+const read_input = (input:string): string[] => {
+    return input.split("\n")
+}
+
+const move = (current: number, direction: "LEFT" | "RIGHT", rotations: number):number => {
+    switch(direction) {
+        case "LEFT": {
+            if (rotations > current) return 100-(rotations-current)
+            return current-rotations
+        }
+        case "RIGHT":
+            if (current+rotations>99) return rotations-(100-current)
+            return current+rotations
+    }
 }
 
 export const run = () => {
@@ -17,3 +48,28 @@ export const run = () => {
 if (import.meta.main) {
     run();
 }
+
+
+Deno.test("move", () => {
+    assertEquals(move(11,"RIGHT",8), 19)
+    assertEquals(move(19,"LEFT",19), 0)
+    assertEquals(move(5,"LEFT",10), 95)
+    assertEquals(move(95,"RIGHT",5), 0)
+    assertEquals(move(99,"RIGHT",1), 0)
+    assertEquals(move(99,"RIGHT",5), 4)
+})
+
+
+Deno.test("read_input", () => {
+    const res = read_input(`L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`)
+assertEquals(res.length,10)
+})
--- Day01/day01.test.ts
+++ Day01/day01.test.ts
@@ -1,19 +1,23 @@
 import { assertEquals } from "@std/assert";
 import { part_1, part_2 } from "./day01.solution.ts";
 
-const test_input = `3   4
-4   3
-2   5
-1   3
-3   9
-3   3`
+const test_input = `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`
 
 Deno.test("part_1", () => {
     const res = part_1(test_input)
-    assertEquals(res, 1)
+    assertEquals(res, 3)
 })
 
 Deno.test("part_2", () => {
     const res = part_2(test_input)
-    assertEquals(res, 1)
+    assertEquals(res, 0)
 })
