442aae1ae9b30eb6c64d4e5a03f155c4ce910238
Gustav Lillesæter <gustav.lillesater@gmail.com>
2025-12-03 21:52:42 +0100

    day 3 part 2

--- Day03/day03.solution.ts
+++ Day03/day03.solution.ts
@@ -1,68 +1,46 @@
 import { assertEquals } from "@std/assert/equals";
-import { input } from "../Day03/day03.input.ts";
+import { input } from "./day03.input.ts";
 
 
-const part_1 = (input:string):number => {
+const part_1 = (input: string): number => {
     const lines = input.split("\n")
-    return lines.reduce((accumulator, currentValue) => accumulator+check_largest_joltage(currentValue), 0)
-    
+    return lines.reduce((accumulator, currentValue) => accumulator + check_largest_joltage(currentValue, 2), 0)
 }
 
-const part_2 = (input:string):number => {
-    return 0
+const part_2 = (input: string): number => {
+    const lines = input.split("\n")
+    return lines.reduce((accumulator, currentValue) => accumulator + check_largest_joltage(currentValue, 12), 0)
 }
 
-const check_largest_joltage_12 = (battery:string):number => {
-    const MAX_LENGTH = 12
-    let largest: number[] = []
+const check_largest_joltage = (battery: string, MAX_LENGTH: number): number => {
+    let largest: number[] = Array(MAX_LENGTH).fill(0, 0)
     const levels = battery.split("").map(Number)
-
-    //find max start
-    const max = levels.slice(0,levels.length-MAX_LENGTH).reduce((accumulator,currentValue) => accumulator < currentValue ? currentValue : accumulator)
-    const maxIndex = levels.findIndex(v => v == max)
-    largest = levels.slice(maxIndex,maxIndex+MAX_LENGTH)
-
-    console.log(levels.slice(maxIndex+MAX_LENGTH))
-
+    let last_idx = 0
+    for (let i = 0; i < MAX_LENGTH; i++) {
+        let candidates = levels.slice(last_idx, levels.length - (MAX_LENGTH - i - 1))
+        let largestIdx = last_idx
+        for (let j = 0; j < candidates.length; j++) {
+            if (candidates[j] > levels[largestIdx]) {
+                largestIdx = last_idx + j
+            }
+        }
+        largest[i] = levels[largestIdx]
+        last_idx = largestIdx + 1
+    }
     return Number(largest.join(""))
 }
 
-
-const check_largest_joltage = (battery: string):number => {
-    const levels = battery.split("").map(Number)
-
-    let largest: number | undefined = undefined
-    let second_largest: number | undefined = undefined
-
-    levels.forEach(level => {
-        if(largest === undefined) largest = level
-        else if (second_largest === undefined) second_largest = level
-        else if (second_largest > largest) {
-            largest = second_largest
-            second_largest = level
-        }
-        else if (level > second_largest) {
-            second_largest = level
-        }
-    })
-
-
-    return Number(`${largest}${second_largest}`)
-} 
-
-
-
 Deno.test("check_largest_joltage_12", () => {
-    assertEquals(check_largest_joltage_12("987654321111111"), 987654321111)
-    assertEquals(check_largest_joltage_12("811111111111119"), 811111111119)
-    assertEquals(check_largest_joltage_12("234234234234278"), 434234234278)
-    assertEquals(check_largest_joltage_12("818181911112111"), 888911112111)
+    assertEquals(check_largest_joltage("987654321111111", 12), 987654321111)
+    assertEquals(check_largest_joltage("811111111111119", 12), 811111111119)
+    assertEquals(check_largest_joltage("234234234234278", 12), 434234234278)
+    assertEquals(check_largest_joltage("818181911112111", 12), 888911112111)
 })
 Deno.test("check_largest_joltage", () => {
-    assertEquals(check_largest_joltage("987654321111111"), 98)
-    assertEquals(check_largest_joltage("811111111111119"), 89)
-    assertEquals(check_largest_joltage("234234234234278"), 78)
-    assertEquals(check_largest_joltage("818181911112111"), 92)
+    assertEquals(check_largest_joltage("987654321111111", 2), 98)
+    assertEquals(check_largest_joltage("811111111111119", 2), 89)
+    assertEquals(check_largest_joltage("234234234234278", 2), 78)
+    assertEquals(check_largest_joltage("818181911112111", 2), 92)
 })
 
 Deno.test("part_1_input", () => {
@@ -71,7 +49,7 @@ Deno.test("part_1_input", () => {
 234234234234278
 818181911112111`
 
-    assertEquals(part_1(input),357)
+    assertEquals(part_1(input), 357)
 })
 
 Deno.test("part_2_input", () => {
@@ -79,11 +57,11 @@ Deno.test("part_2_input", () => {
 811111111111119
 234234234234278
 818181911112111`
-    
-        assertEquals(part_2(input),3121910778619)
+
+    assertEquals(part_2(input), 3121910778619)
 })
 
 if (import.meta.main) {
-    console.log("part 1:",part_1(input))
-    console.log("part 2:",part_2(input))
+    console.log("part 1:", part_1(input))
+    console.log("part 2:", part_2(input))
 }
