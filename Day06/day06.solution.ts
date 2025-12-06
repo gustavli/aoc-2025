e9dc5c511fd32078aa1d794e00dbe44d4cd0448f
Gustav Lillesæter <gustav.lillesater@gmail.com>
2025-12-06 15:45:34 +0100

    day 6 part 2

--- Day06/day06.solution.ts
+++ Day06/day06.solution.ts
@@ -2,7 +2,7 @@ import { assertEquals } from "@std/assert/equals";
 import { input } from "./day06.input.ts";
 
 const part_1 = (input: string): number => {
-    const lines = input.split("\n").map(line => line.trim().split("/\s+/"))
+    const lines = input.split("\n").map(line => line.trim().split(/\s+/))
 
     let sum = 0
 
@@ -19,15 +19,39 @@ const part_1 = (input: string): number => {
 
         sum += line_answer
     }
-
     return sum
 }
 
 
 const part_2 = (input: string): number => {
     const lines = input.split("\n")
-    console.log(lines)
+    const regex = new RegExp("[\*\+]", "gi")
+    const modes = lines[lines.length - 1]
+    const number_starts = [...modes.matchAll(regex).map(a => a.index)]
+
     let result = 0
+
+    for (let i = 0; i < number_starts.length; i++) {
+        const mode = modes[number_starts[i]]
+
+        const from = number_starts[i]
+        const to = i === number_starts.length - 1 ? lines[0].length - 1 : number_starts[i + 1] - 2
+
+        let line_result = mode === "*" ? 1 : 0
+        for (let j = from; j <= to; j++) {
+            const new_num_arr: string[] = []
+            for (let idx = 0; idx < lines.length - 1; idx++) {
+
+                const c = lines[idx][j]
+                if (c !== " ") {
+                    new_num_arr.push(c)
+                }
+            }
+            const new_num = Number(new_num_arr.join(""))
+            line_result = add_or_multiply(mode, line_result, new_num)
+        }
+        result += line_result
+    }
 
     return result
 }
